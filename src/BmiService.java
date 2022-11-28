1a19d9ef16edcec5f7c87fbc50069b7fbb293336
Barbara Makarova <makarena2001barbara@yandex.ru>
2022-11-28 14:05:39 +0300

    change for homework

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public double calculate(double weight, double height) {
-        double index = weight/(height*height);
+        double index = weight / (height * height);
         return index;
     }
 }
